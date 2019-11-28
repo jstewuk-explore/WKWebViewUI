2f160d94a34d1915691f78da83ca34edfc80e1e9
Jim Stewart <jim@stewartstuff.com>
2019-11-28 11:54:07 -0600

    Initial Commit

---
+++ WKWebViewUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WKWebViewUI
+//
+//  Created by James Stewart on 11/28/19.
+//  Copyright © 2019 James Stewart. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
